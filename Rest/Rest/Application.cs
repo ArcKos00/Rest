005d82206d3879b2d27ddd82501b92d96b4b675b
ArcKos00 <105163313+ArcKos00@users.noreply.github.com>
2022-12-04 19:46:08 +0200

    parcer

--- Rest/Rest/Application.cs
+++ Rest/Rest/Application.cs
@@ -30,48 +30,113 @@ namespace RestRequest
         public async Task Start()
         {
             var user2 = await _userService.GetUserAsync(2);
-            _logger.LogInformation(JsonConvert.SerializeObject(user2));
+            _logger.LogInformation(JsonParcer(JsonConvert.SerializeObject(user2)));
 
             var usersList = await _userService.GetListUsersOnPageAsync(2);
-            _logger.LogInformation(JsonConvert.SerializeObject(usersList));
+            _logger.LogInformation(JsonParcer(JsonConvert.SerializeObject(usersList)));
 
             var delayUsersList = await _userService.UserDelayAsync(3);
-            _logger.LogInformation(JsonConvert.SerializeObject(delayUsersList));
+            _logger.LogInformation(JsonParcer(JsonConvert.SerializeObject(delayUsersList)));
 
             var user23 = await _userService.GetUserAsync(23);
-            _logger.LogInformation(JsonConvert.SerializeObject(user23));
+            _logger.LogInformation(JsonParcer(JsonConvert.SerializeObject(user23)));
 
             var listResource = await _resourceService.GetAllResourcesAsync();
-            _logger.LogInformation(JsonConvert.SerializeObject(listResource));
+            _logger.LogInformation(JsonParcer(JsonConvert.SerializeObject(listResource)));
 
             var singleResource = await _resourceService.GetResourceAsync(2);
-            _logger.LogInformation(JsonConvert.SerializeObject(singleResource));
+            _logger.LogInformation(JsonParcer(JsonConvert.SerializeObject(singleResource)));
 
             var singleResourceNotFound = await _resourceService.GetResourceAsync(23);
-            _logger.LogInformation(JsonConvert.SerializeObject(singleResourceNotFound));
+            _logger.LogInformation(JsonParcer(JsonConvert.SerializeObject(singleResourceNotFound)));
 
             var resultMike = await _userService.CreateUserAsync("Mike", "SysAdmin");
-            _logger.LogInformation(JsonConvert.SerializeObject(resultMike));
+            _logger.LogInformation(JsonParcer(JsonConvert.SerializeObject(resultMike)));
 
             if (resultMike != null)
             {
                 var updateUser = await _userService.UpdateUserAsync("morpheus", "zion resident", resultMike.Id);
-                _logger.LogInformation(JsonConvert.SerializeObject(updateUser));
+                _logger.LogInformation(JsonParcer(JsonConvert.SerializeObject(updateUser)));
 
                 await _userService.DeleteUserAsync(resultMike.Id);
             }
 
             var registerSuccessful = await _authenticationService.RegisterAsync("eoe@ggwp.com", "riotgames");
-            _logger.LogInformation(JsonConvert.SerializeObject(registerSuccessful));
+            _logger.LogInformation(JsonParcer(JsonConvert.SerializeObject(registerSuccessful)));
 
             var registerUnuccessful = await _authenticationService.RegisterAsync("eoe@ggwp.com");
-            _logger.LogInformation(JsonConvert.SerializeObject(registerUnuccessful));
+            _logger.LogInformation(JsonParcer(JsonConvert.SerializeObject(registerUnuccessful)));
 
             var loginSuccessful = await _authenticationService.LoginAsync("eoe@ggwp.com", "riotgames");
-            _logger.LogInformation(JsonConvert.SerializeObject(loginSuccessful));
+            _logger.LogInformation(JsonParcer(JsonConvert.SerializeObject(loginSuccessful)));
 
             var loginUnuccessful = await _authenticationService.LoginAsync("eoe@ggwp.com");
-            _logger.LogInformation(JsonConvert.SerializeObject(loginSuccessful));
+            _logger.LogInformation(JsonParcer(JsonConvert.SerializeObject(loginSuccessful)));
+        }
+
+        private string JsonParcer(string json)
+        {
+            var counter = 0;
+            var tab = new StringBuilder();
+            var sb = new StringBuilder();
+            var dic = new Dictionary<char, string>()
+            {
+                ['{'] = '{' + Environment.NewLine,
+                ['}'] = Environment.NewLine + '}' + Environment.NewLine,
+                ['['] = "[" + Environment.NewLine,
+                [']'] = Environment.NewLine + "]" + Environment.NewLine,
+                [','] = ',' + Environment.NewLine
+            };
+
+            for (int i = 0; i < json.Length; i++)
+            {
+                tab.Clear();
+
+                if (json[i] == ',')
+                {
+                    for (int j = 0; j < counter; j++)
+                    {
+                        tab.Append("   ");
+                    }
+
+                    sb.Append(dic[json[i]]).Append(tab.ToString());
+                    continue;
+                }
+
+                if (json[i] == '{' || json[i] == '[')
+                {
+                    counter++;
+                    for (int j = 0; j < counter; j++)
+                    {
+                        tab.Append("   ");
+                    }
+
+                    sb.Append(dic[json[i]]).Append(tab.ToString());
+                    continue;
+                }
+
+                if (json[i] == '}' || json[i] == ']')
+                {
+                    counter--;
+                    for (int j = 0; j < counter; j++)
+                    {
+                        tab.Append("   ");
+                    }
+
+                    if (i < json.Length - 1 && json[i + 1] == ',')
+                    {
+                        sb.Append(json[i]);
+                        continue;
+                    }
+
+                    sb.Append(dic[json[i]]).Append(tab.ToString());
+                    continue;
+                }
+
+                sb.Append(json[i]);
+            }
+
+            return sb.ToString();
         }
     }
 }
