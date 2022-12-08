f14efd4f209dd346954503679005a4983f5cca4a
ArcKos00 <105163313+ArcKos00@users.noreply.github.com>
2022-12-08 22:38:26 +0200

    done

--- Rest/Rest/Services/Abstractions/IUserService.cs
+++ Rest/Rest/Services/Abstractions/IUserService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Rest.Dtos.Response;
 using RestRequest.Dtos;
 using RestRequest.Dtos.Response;
 
@@ -10,11 +11,11 @@ namespace RestRequest.Services.Abstractions
 {
     public interface IUserService
     {
-        public Task<BaseResponse<List<UserDto>>?> GetListUsersOnPageAsync(int page);
+        public Task<Pager<List<UserDto>>?> GetListUsersOnPageAsync(int page);
         public Task<BaseResponse<UserDto>?> GetUserAsync(int id);
         public Task<UserResponse?> CreateUserAsync(string name, string job);
         public Task<UserResponse?> UpdateUserAsync(string name, string job, int id);
         public Task DeleteUserAsync(int id);
-        public Task<BaseResponse<List<UserDto>>?> UserDelayAsync(int page);
+        public Task<Pager<List<UserDto>>?> UserDelayAsync(int page);
     }
 }
